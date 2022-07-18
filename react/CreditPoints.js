3895f6989db5dc3b71f4fc9b5294255e0ea2c981
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-07-18 15:12:17 -0300

    consumo de api

---
+++ react/CreditPoints.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import api from './api';
+
+class CreditPoints extends Component {
+
+    state = {
+        filmes: [],
+      }
+    
+      async componentDidMount() {
+        const response = await api.get('star%20wars');
+    
+        this.setState({ filmes: response.data });
+      }
+    
+      render() {
+    
+        const { filmes } = this.state;
+    
+        return (
+          <div>
+            <h1>Listar os Filmes</h1>
+            {filmes.map(filme => (
+              <li key={filme.show.id}>
+                <h2>
+                  <strong>Título: </strong>
+                  {filme.show.name}
+                </h2>
+                <p>
+                  {filme.show.url}
+                </p>
+    
+              </li>
+            ))}
+          </div>
+        );
+      };
+    };
+
+
+
+export default CreditPoints
