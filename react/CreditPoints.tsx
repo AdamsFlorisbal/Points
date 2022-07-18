f04611508e2f5cc19161be0f18d44b6e618fd74e
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-07-17 21:43:22 -0300

    Primeiro render

---
+++ react/CreditPoints.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CreditPoints = () => {
+    return (
+        <div>
+            <h1>
+                test
+            </h1>
+
+
+        </div>
+    )
+
+
+}
+
+export default CreditPoints
