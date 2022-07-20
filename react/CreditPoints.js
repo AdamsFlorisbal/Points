da81b1825236693689ffe1ce080903b2521b2b70
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-07-20 15:34:11 -0300

    points

--- react/CreditPoints.js
+++ react/CreditPoints.js
@@ -3,36 +3,29 @@ import api from './api';
 
 class CreditPoints extends Component {
 
-    state = {
-        filmes: [],
-      }
-    
-      async componentDidMount() {
-        const response = await api.get('star%20wars');
-    
-        this.setState({ filmes: response.data });
-      }
-    
-      render() {
-    
-        const { filmes } = this.state;
-    
-        return (
-          <div>
-            <h1>Listar os Filmes</h1>
-            {filmes.map(filme => (
-              <li key={filme.show.id}>
-                <h2>
-                  <strong>Pontos: </strong>
-                  {filme.show.name}
-                </h2>
-                   
-              </li>
-            ))}
-          </div>
-        );
-      };
-    };
+  state = {
+    filmes: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('star%20wars');
+
+    this.setState({ filmes: response.data });
+  }
+
+  render() {
+
+    const { filmes } = this.state;
+
+    return (
+      <div>
+        <h2>
+          <strong>Pontos: 1000</strong>
+        </h2>
+      </div>
+    );
+  };
+};
 
 
 
